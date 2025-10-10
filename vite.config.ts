41220f92c0bd4c5547e79cc7c41033b9655e283c
CaesarTheDay <44045297+Caesar-The-Day@users.noreply.github.com>
2025-10-10 16:48:24 -0700

    Update vite.config.ts
    update vite component

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 👇 crucial for GitHub Pages project sites
+  base: "/ctd-newsletter/",
   server: {
     host: "::",
     port: 8080,
